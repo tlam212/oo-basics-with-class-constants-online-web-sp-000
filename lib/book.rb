1307f7a5ad752f94ef815c7ee5eca994383573d6
Toni L <tlam212@gmail.com>
2019-08-29 22:08:38 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -5,8 +5,10 @@ class Book
   attr_accessor :author, :page_count, :genre
   attr_reader :title
 
-  def initialize(title)
+  def initialize(title, genre)
+    @genre =  genre
     @title = title
+    GENRES << all
   end
 
   def turn_page
